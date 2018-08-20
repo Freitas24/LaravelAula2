48bda840de7b817b38368b93e9c96388a1a30a9d
ifrs <ifrs>
2018-08-20 15:59:35 -0300

    meio da aula

--- resources/views/mensagem/list.blade.php
+++ resources/views/mensagem/list.blade.php
@@ -1,6 +1,7 @@
 <h1>Lista de mensagens</h1>
 <hr>
 @foreach($mensagens as $mensagem)
+	<h3>{{$mensagens->scheduledto}}</h3>
 	<p><a href="/mensagens/{{$mensagem->id}}">{{$mensagem->titulo}}</a></p>
 	<p>{{$mensagem->autor}}</p>
 	<br>
